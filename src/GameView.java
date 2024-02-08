9803a1892c535eeccc6071f0bcb8c5b5994eb66b
sabrinavohra <sabrina.vohra@menloschool.org>
2024-02-08 09:18:09 -0800

    Paint

--- src/GameView.java
+++ src/GameView.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
-import java.awt.Image;
+import java.awt.*;
+
 public class GameView extends JFrame {
     private static final int WINDOW_HEIGHT = 600;
     private static final int WINDOW_WIDTH = 1000;
     private Image[] diceImages;
     private Game g;
+    private String currentState;
 
     public GameView(Game g) {
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setTitle("Dice Game!");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.currentState = "Intro";
+    }
+
+    public void paint(Graphics g) {
+        if(currentState.equals("Intro")) {
+            g.setColor(Color.white);
+            g.fillRect(600, 1000, 600, 1000);
+        }
     }
 }
